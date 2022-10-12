d3dbff663bad3a1677ca5eefe3692492a47a2a2d
ClaudiaV27 <claudiavielmetti38@gmail.com>
2022-10-12 13:26:53 +0200

    Start of JS

---
+++ main.js
@@ -0,0 +1,35 @@
+
+//Adding a global variable that can be used throughout the project
+//with local storage
+//the name input is set to store to the local storage
+// e is to parse though an event
+window.addEventListener('load', () => {
+    todos = JSON.parse(localStorage.getItem('todos')) || [];
+    const nameInput = document.querySelector('#name');
+    const newTask = document.querySelector('#new-task'); 
+
+    const username = localStorage.getItem('username') || '';
+
+    nameInput.value = username;
+
+    nameInput.addEventListener('change', e => {
+        localStorage.setItem('username', e.target.value);
+    })
+
+    //getting the JSON code
+
+    newTask.addEventListener('submit', e => {
+        e.preventDefault();
+
+        const todo={
+            content: e.target.elements.content.value,
+            category:e.target.elements.category.value,
+            done: false,
+            
+
+        }
+
+    })
+
+})
+

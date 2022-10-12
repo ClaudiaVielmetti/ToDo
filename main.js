4d8fa8e04e6ff41fa460f0518a1db758c113a9c1
ClaudiaV27 <claudiavielmetti38@gmail.com>
2022-10-12 14:16:20 +0200

    inputs updated

--- main.js
+++ main.js
@@ -32,7 +32,61 @@ window.addEventListener('load', () => {
         localStorage.setItem('todos', JSON.stringify(todos));
 
         e.target.reset();
+
+        DisplayTodos();
     })
 
 })
 
+function DisplayTodos(){
+    const todoList = document.querySelector('#todo-list');
+
+    todoList.innerHTML = '';
+
+    todos.forEach(todo => {
+        const todoItem = document.createElement('div');
+        todoItem.classList.add('todo-item')
+
+        const label = document.createElement('label');
+        const input = document.createElement('input');
+        const span = document.createElement('span');
+        const content = document.createElement('div');
+        const actions = document.createElement('div');
+        const edit = document.createElement('button');
+        const deleteButton = document.createElement('button');
+
+        input.type='checkbox';
+        input.checked = todo.done;
+        span.classList.add('bubble');
+
+        if (todo.category == 'personal'){
+            span.classList.add('personal');
+
+        }else {
+            span.classList.add('work');
+        }
+
+        content.classList.add('todo-content');
+        actions.classList.add('actions');
+        edit.classList.add('edit');
+        deleteButton.classList.add('delete');
+
+        
+
+		content.innerHTML = `<input type="text" value="${todo.content}" readonly>`;
+		edit.innerHTML = 'Edit';
+		deleteButton.innerHTML = 'Delete';
+
+        label.appendChild(span);
+        actions.appendChild(edit);
+        actions.appendChild(deleteButton);
+        todoItem.appendChild(label);
+        todoItem.appendChild(content);
+        todoItem.appendChild(actions);
+
+        todoList.appendChild(todoItem);
+        
+        if (todo.done) {
+			todoItem.classList.add('done');
+    })
+}

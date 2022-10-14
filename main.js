c9e9f34a8f93e25739eb3bf7a8a34d1fa973d566
ClaudiaV27 <claudiavielmetti38@gmail.com>
2022-10-14 11:47:12 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,135 +1,45 @@
+let form = document.getElementById("form");
+let textInput = document.getElementById("textInput");
+let dateInput = document.getElementById("dateInput");
+let textarea = document.getElementById("textarea");
+let msg = document.getElementById("msg");
+let tasks = document.getElementById("tasks");
+let add = document.getElementById("add");
 
-//Adding a global variable that can be used throughout the project
-//with local storage
-//the name input is set to store to the local storage
-// e is to parse though an event
-window.addEventListener('load', () => {
-    todos = JSON.parse(localStorage.getItem('todos')) || [];
-    const nameInput = document.querySelector('#name');
-    const newTask = document.querySelector('#new-task'); 
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    formValidation();
 
-    const username = localStorage.getItem('username') || '';
+ });
 
-    nameInput.value = username;
+let formValidation = () => {
+    if (textInput.value === "") {
+      console.log("failure");
+      msg.innerHTML = "Task cannot be blank";
+    } else {
+      console.log("success");
+      msg.innerHTML = "";
+      acceptData();
+      add.setAttribute("data-bs-dismiss", "modal");
+      add.click();
+  
+      (() => {
+        add.setAttribute("data-bs-dismiss", "");
+      })();
+    }
+};
 
-    nameInput.addEventListener('change', e => {
-        localStorage.setItem('username', e.target.value);
-    })
+let data = [{}];
+  
+let acceptData = () => {
+  data.push({
+	text: textInput.value,
+	date: dateInput.value,
+	description: textarea.value,
+  });
 
-    //getting the JSON code
+  localStorage.setItem("data", JSON.stringify(data));
 
-    newTask.addEventListener('submit', e => {
-        e.preventDefault();
-
-        
-
-        const todo = {
-			content: e.target.elements.content.value,
-			category: e.target.elements.category.value,
-			done: false,
-			createdAt: new Date().getTime()
-		}
-
-		todos.push(todo);
-
-		localStorage.setItem('todos', JSON.stringify(todos));
-
-		// Reset the form
-		e.target.reset();
-
-		DisplayTodos()
-	})
-
-	DisplayTodos()
-})
-
-//displaying items on screen that has been inputted
-
-function DisplayTodos () {
-	const todoList = document.querySelector('#todo-list');
-	todoList.innerHTML = "";
-
-	todos.forEach(todo => {
-		const todoItem = document.createElement('div');
-		todoItem.classList.add('todo-item');
-
-		const label = document.createElement('label');
-		const input = document.createElement('input');
-		const span = document.createElement('span');
-		const content = document.createElement('div');
-		const actions = document.createElement('div');
-		const edit = document.createElement('button');
-		const deleteButton = document.createElement('button');
-
-        //differentiate between categories
-
-		input.type = 'checkbox';
-		input.checked = todo.done;
-		span.classList.add('bubble');
-		if (todo.category == 'personal') {
-			span.classList.add('personal');
-		} else {
-			span.classList.add('business');
-		}
-		content.classList.add('todo-content');
-		actions.classList.add('actions');
-		edit.classList.add('edit');
-		deleteButton.classList.add('delete');
-
-		content.innerHTML = `<input type="text" value="${todo.content}" readonly>`;
-		edit.innerHTML = 'Edit';
-		deleteButton.innerHTML = 'Delete';
-
-        // appendChild() moves it from its current position to the new position.
-
-		label.appendChild(input);
-		label.appendChild(span);
-		actions.appendChild(edit);
-		actions.appendChild(deleteButton);
-		todoItem.appendChild(label);
-		todoItem.appendChild(content);
-		todoItem.appendChild(actions);
-
-		todoList.appendChild(todoItem);
-
-		if (todo.done) {
-			todoItem.classList.add('done');
-		}
-		
-		input.addEventListener('change', (e) => {
-			todo.done = e.target.checked;
-			localStorage.setItem('todos', JSON.stringify(todos));
-
-			if (todo.done) {
-				todoItem.classList.add('done');
-			} else {
-				todoItem.classList.remove('done');
-			}
-
-			DisplayTodos()
-
-		})
-
-        //delete and edit buttons for task list.. remove or make changes
-
-		edit.addEventListener('click', (e) => {
-			const input = content.querySelector('input');
-			input.removeAttribute('readonly');
-			input.focus();
-			input.addEventListener('blur', (e) => {
-				input.setAttribute('readonly', true);
-				todo.content = e.target.value;
-				localStorage.setItem('todos', JSON.stringify(todos));
-				DisplayTodos()
-
-			})
-		})
-
-		deleteButton.addEventListener('click', (e) => {
-			todos = todos.filter(t => t != todo);
-			localStorage.setItem('todos', JSON.stringify(todos));
-			DisplayTodos()
-		})
-
-	})
-}
+  console.log(data);
+  createTasks();
+};
